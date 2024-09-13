d27c8a426ea9877b4b845e8a53ba26682682efa5
Aditya kumar <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2024-09-13 18:50:06 +0530

    hmjhnvh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="footer-content">
           {/* Simplified Contact and About Section */}
           <div className="footer-info">
-            <p>Email: fashionhub@gmail.com | Phone: 12jkjhg3-456-7890</p>
+            <p>Email: fashionhub@gmail.com | Phone: 123-456-7890</p>
             <p>&copy; 2024 Fashion Hub. ADITYA KUMAR</p>
           </div>
         </div>
